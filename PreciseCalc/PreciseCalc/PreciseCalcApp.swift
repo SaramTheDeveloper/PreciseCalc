2f74daecdc8f38f35e8c2aa8fcb4a7a977ae68a0
SaramTheDeveloper <118079805+SaramTheDeveloper@users.noreply.github.com>
2023-01-29 16:08:46 -0500

    Add files via upload
    Calculator App that allows add, subtract, multiply and divide

---
+++ PreciseCalc/PreciseCalc/PreciseCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreciseCalcApp.swift
+//  PreciseCalc
+//
+//  Created by Saram Hayat on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct PreciseCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
